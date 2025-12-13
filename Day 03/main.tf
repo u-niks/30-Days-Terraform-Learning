c74ac2961af1aa7af35af31364478dc7598cbb86
Ubuntu <ubuntu@ip-172-31-46-17.ec2.internal>
2025-12-13 08:33:05 +0000

    Day 03 Task

---
+++ Day 03/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo-bucket" {
+  bucket = "my-tf-demo-bucket"
+
+  tags = {
+    Name        = "Demo Bucket"
+    Environment = "DEV"
+  }
+}
