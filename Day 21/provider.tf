7a75b36fbe6d650511d1b3012e47c76d5fc17519
Ubuntu <jadhavnikhil826@gmail.com>
2026-01-29 08:55:56 +0000

    AWS Provider and Setting up Backend Bucket for .tfstate file

---
+++ Day 21/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+    backend "s3" {
+        bucket       = "demo-terraform-state-bucket-001"
+        key          = "dev/terraform.tfstate"
+        region       = "us-east-1"
+        encrypt      = true
+        use_lockfile = true
+    }
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+
+        random = {
+            source  = "hashicorp/random"
+            version = "~> 3.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = var.region
+}
