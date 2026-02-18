c4226d5b15c60d3e4e5a0b121fb13776caf9fe0d
Ubuntu <jadhavnikhil826@gmail.com>
2026-02-18 08:43:31 +0000

    Security Group Configs

---
+++ Day 27/provider.tf
@@ -0,0 +1,31 @@
+terraform {
+    backend "s3" {
+        bucket       = "demo-terraform-state-bucket-000111"
+        key          = "terraform/state/main/terraform.tfstate"
+        region       = "us-east-1"
+        encrypt      = true
+        use_lockfile = true
+    }
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+        random = {
+            source  = "hashicorp/random"
+            version = "~> 3.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = var.region 
+
+    default_tags {
+        tags = {
+            Environment = var.environment
+            Project     = "AWS-Production-Infrastructure"
+            ManagedBy   = "Terraform"
+        }
+    }
+}
