8cb43cab151f38284879b27a265e57e263212068
Ubuntu <jadhavnikhil826@gmail.com>
2026-02-18 09:26:50 +0000

    Terraform AWS Provider Configuration

--- Day 27/provider.tf
+++ Day 27/provider.tf
@@ -1,4 +1,5 @@
 terraform {
+    required_version = ">= 1.0.0"
     backend "s3" {
         bucket       = "demo-terraform-state-bucket-000111"
         key          = "terraform/state/main/terraform.tfstate"
