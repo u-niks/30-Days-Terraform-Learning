3c544717039bf9a4697cc42f9595cdee8062d5d2
Nikhil Jadhav <jadhavnikhil826@gmail.com>
2026-02-26 14:37:18 +0530

    Fix alb target_group health_check code

--- Day 28/terraform-infra/modules/alb/main.tf
+++ Day 28/terraform-infra/modules/alb/main.tf
@@ -34,7 +34,7 @@ resource "aws_lb_target_group" "main" {
         unhealthy_threshold = 3
         timeout             = 5
         interval            = 30
-        matcher             = 200-299
+        matcher             = "200-299"
     }
 
     deregistration_delay = 30
