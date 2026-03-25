3de42a4068709faf01b198f604bb6ad81655838e
Nikhil Jadhav <jadhavnikhil826@gmail.com>
2026-03-25 14:51:17 +0530

    updated terraform backend.tf

--- Day 29/terraform/backend.tf
+++ Day 29/terraform/backend.tf
@@ -19,8 +19,8 @@ terraform {
         }
 
         kubectl = {
-            source  = "gavinbunney/kubectl"
-            version = ">= 1.14.0"
+            source = "alekc/kubectl"
+            version = ">= 2.0.0"
         }
 
         http = {
