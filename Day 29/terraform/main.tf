07a0e91e63f7cbe724f75adb8890a6f225e89697
Nikhil Jadhav <jadhavnikhil826@gmail.com>
2026-03-25 14:56:42 +0530

    updated terraform main.tf

--- Day 29/terraform/main.tf
+++ Day 29/terraform/main.tf
@@ -72,20 +72,17 @@ module "eks" {
 # EBS CSI Driver IAM Role
 module "ebs_csi_driver_irsa" {
     source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-    version = "~> 5.0"
+    version = "~> 5.20"
 
     role_name_prefix      = "ebs-csi-driver-"
     attach_ebs_csi_policy = true
 
-    oidc_providers = {
+      oidc_providers = {
         main = {
-            provider_arn = module.eks.oidc_provider_arn
-            service_accounts = [{
-                namespace = "kube-system"
-                name      = "ebs-csi-controller-sa"
-            }]
+          provider_arn               = module.eks_cluster.oidc_provider_arn
+          namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
         }
-    }
+      }
 
     tags = {
         Environment = "dev"
