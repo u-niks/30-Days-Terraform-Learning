11b27b0de08adb9f636e35508f580ede2c57bfbd
Nikhil Jadhav <jadhavnikhil826@gmail.com>
2026-03-25 16:15:12 +0530

    updated backend.tf

--- Day 29/terraform/backend.tf
+++ Day 29/terraform/backend.tf
@@ -4,7 +4,6 @@ terraform {
         key          = "terraform/state/main/terraform.tfstate"
         region       = "us-east-1"
         encrypt      = true
-        use_lockfile = true
     }
 
     required_providers {
